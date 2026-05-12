27a414d84edceead1e3c6b59cfddc2fdfc634fc2
prakash0304 <prakash0304@gmail.com>
2026-05-12 19:30:42 +0530

    main&input

---
+++ Input.py
@@ -0,0 +1,11 @@
+name=input("what is your name:")
+age=input("age is:")
+
+
+
+print("hello my name is {name}")
+print("i am {age} years old\n\n\n")
+
+
+print(f"hello my name is {name}")
+print(f"i am {age} years old")
---
+++ main.py
@@ -0,0 +1,7 @@
+#age=25
+#print(f"{age}")
+#print(age)
+a=25
+b=3467.5664646445
+b=int(b)
+print(b)
