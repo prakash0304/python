c0cf9935cbc6e7aa06fa50420d134f45d2211cec
prakash0304 <prakash0304@gmail.com>
2026-05-14 19:56:06 +0530

    values assingn

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,9 @@
-# Program to display the Fibonacci sequence up to n-th term
 nterms = int(input("How many terms? "))
 
-# First two terms
 n1, n2 = 0, 1
 count = 0
 
-# Check if the number of terms is valid
+
 if nterms <= 0:
     print("Please enter a positive integer")
 elif nterms == 1:
@@ -15,7 +13,7 @@ else:
     while count < nterms:
         print(n1, end=" ")
         nth = n1 + n2
-        # Update values
+
         n1 = n2
         n2 = nth
         count += 1
---
+++ value.py
@@ -0,0 +1,6 @@
+n1,n2=1,3
+n3=n1+n2
+n1=n2
+n2=n3
+print(n3,n2,n1)
+print(n1+n2)
